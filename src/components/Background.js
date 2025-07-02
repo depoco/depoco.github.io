8709f0b467c62676d67b4b232587c6be38b11c41
baguettebyte <thebaguettebyte@gmail.com>
2025-07-02 16:52:18 +0200

    Initial commit

---
+++ src/components/Background.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Background.css';
+
+function Background() {
+  return (
+    <div className="background-animation">
+      {/* Subtle animated elements can go here */}
+    </div>
+  );
+}
+
+export default Background;
